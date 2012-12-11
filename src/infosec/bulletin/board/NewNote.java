88cbd8e9f10c4f2965f0317f26faf9f9afd2a7d1
fiend91206 <fiend91206@gmail.com>
2012-12-11 15:20:08 -0500

    Added UI for the 2 pages

--- src/infosec/bulletin/board/NewNote.java
+++ src/infosec/bulletin/board/NewNote.java
@@ -1,6 +1,9 @@
 package infosec.bulletin.board;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 
 public class NewNote extends Activity {
@@ -9,6 +12,19 @@ public class NewNote extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_note);
 		
+		Button cancel = (Button) findViewById(R.id.cancel);
+		Button save = (Button) findViewById(R.id.save);
+		TextView noteField = (TextView) findViewById(R.id.note);
 		
+		cancel.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		
+		save.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			}
+		});
 	}
 }
